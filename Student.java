72dde00c888ec1d691827774a63c2fb6bd6fe2ac
David Truong <david.c.truong-1@ou.edu>
2019-10-14 14:03:39 -0500

    Sketched a new class called Student (with rating)

---
+++ Student.java
@@ -0,0 +1,12 @@
+
+public class Student implements Comparable<Student> {
+	private String className;
+	private Integer rating;
+	
+	@Override
+	public int compareTo(Student o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
